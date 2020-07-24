53b10b8b3f98fd207e47b52151b2378c66c76f60
neil.garner <neil.garner@autotrader.co.uk>
2020-07-24 10:15:59 +0100

    More fomatting on recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,39 +2,42 @@ import React from 'react'
 import styled from 'styled-components'
 
 const RecipeContainer = styled.section`
-    margin-top: 20px;
+  margin-top: 20px;
 `
 
 const IngredientsContainer = styled.section`
-    width: 100%;
-    margin: auto;
+  width: 100%;
+  margin: 0px auto 25px auto;
 `
 
 const IngredientContainer = styled.div`
-    width: 80%;
-    display: grid;
-    grid-template-columns: minmax(75px, 25%) 1fr;
+  width: 80%;
+  display: grid;
+  grid-template-columns: minmax(75px, 25%) 1fr;
+  margin-bottom: 5px;
 `
 
 const MethodContainer = styled.div`
-    margin-bottom: 15px;
+  margin-bottom: 15px;
 `
 
-const Ingredient = ({ingredient}) => {
-    return (
-        <IngredientContainer>
-            <span>{ingredient.amount && `${ingredient.amount} ${ingredient.unit}`}</span>
-            <span>{ingredient.name}</span>
-        </IngredientContainer>
-    )
+const Ingredient = ({ ingredient }) => {
+  return (
+    <IngredientContainer>
+      <span>
+        {ingredient.amount && `${ingredient.amount} ${ingredient.unit}`}
+      </span>
+      <span>{ingredient.name}</span>
+    </IngredientContainer>
+  )
 }
 
-const Method = ({method}) => {
-    return (
-        <MethodContainer>
-            <span>{method}</span>
-        </MethodContainer>
-    )
+const Method = ({ method }) => {
+  return (
+    <MethodContainer>
+      <span>{method}</span>
+    </MethodContainer>
+  )
 }
 
 export const Recipe = ({ recipe }) => {
@@ -49,15 +52,17 @@ export const Recipe = ({ recipe }) => {
               <Ingredient key={index} ingredient={recipe} />
             ))}
         </ol>
+      </IngredientsContainer>
+      <section>
         <h3>Method</h3>
         <ol>
           {recipe &&
             recipe.method.length > 0 &&
             recipe.method.map((method, index) => (
-                <Method key={index} method={method} />
+              <Method key={index} method={method} />
             ))}
         </ol>
-      </IngredientsContainer>
+      </section>
     </RecipeContainer>
   )
 }
